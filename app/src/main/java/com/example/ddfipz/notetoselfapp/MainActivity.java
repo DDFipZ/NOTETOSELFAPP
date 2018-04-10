14303e5cc4abbfbd113f7052e6b301ebe18209bf
DDFipZ <marcus2860@gmail.com>
2018-04-10 21:19:28 +0200

    added the menu_main.xml file

--- app/src/main/java/com/example/ddfipz/notetoselfapp/MainActivity.java
+++ app/src/main/java/com/example/ddfipz/notetoselfapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.ddfipz.notetoselfapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.support.v7.app.ActionBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +29,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.action_add) {
+            DialogNewNote dialog = new DialogNewNote();
+            dialog.show(getFragmentManager(), "");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 

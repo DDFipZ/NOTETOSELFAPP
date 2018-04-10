20da14bc9f84145ec3966c7c2b7ef4e0570a4c1d
DDFipZ <marcus2860@gmail.com>
2018-04-11 00:45:23 +0200

    Chapter 14 complete, however there is an issue when trying to show the note, the text is not updated :/

--- app/src/main/java/com/example/ddfipz/notetoselfapp/DialogNewNote.java
+++ app/src/main/java/com/example/ddfipz/notetoselfapp/DialogNewNote.java
@@ -17,8 +17,8 @@ public class DialogNewNote extends DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View dialogView = inflater.inflate(R.layout.dialog_new_note,null);
 
-        final EditText editTitle = (EditText) dialogView.findViewById(R.id.editTitle);
-        final EditText editDescription = (EditText) dialogView.findViewById(R.id.editDescription);
+        final EditText editTitle = (EditText) dialogView.findViewById(R.id.txtTitle);
+        final EditText editDescription = (EditText) dialogView.findViewById(R.id.txtDescription);
         final CheckBox checkBoxIdea = (CheckBox) dialogView.findViewById(R.id.checkBoxIdea);
         final CheckBox checkBoxTodo = (CheckBox) dialogView.findViewById(R.id.checkBoxTodo);
         final CheckBox checkBoxImportant = (CheckBox) dialogView.findViewById(R.id.checkBoxImportant);
--- app/src/main/java/com/example/ddfipz/notetoselfapp/MainActivity.java
+++ app/src/main/java/com/example/ddfipz/notetoselfapp/MainActivity.java
@@ -1,34 +1,67 @@
 package com.example.ddfipz.notetoselfapp;
 
+import android.content.Context;
+import android.media.Image;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.support.v7.app.ActionBar;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class MainActivity extends AppCompatActivity {
 
-    Note mTempNote = new Note();
-    public void createNewNote(Note n){
-        mTempNote = n;
-    }
+private NoteAdapter mNoteAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       //Hello World! 222
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        //Hello World! 222
+
+        mNoteAdapter = new NoteAdapter();
+
+        ListView listNote = (ListView) findViewById(R.id.listView);
+        listNote.setAdapter(mNoteAdapter);
+
+        //TODO this is how to make a listview an onclick listener!!! Apply this in the shopping list app and combine it with the knowledge of the imageviews to be able to select items for deletion
+
+        listNote.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onItemClick(AdapterView<?> adapter, View view, int whichItem, long id) {
+                //create a temp note which is a reference
+                //to the note that has just been clicked
+
+                Note tempNote = mNoteAdapter.getItem(whichItem);
+
+                // create a new dialog window
+
                 DialogShowNote dialog = new DialogShowNote();
-                dialog.sendNoteSelected(mTempNote);
-                dialog.show(getFragmentManager(),"123");
+                dialog.sendNoteSelected(tempNote);
+                //send in a reference to the note to be shown
+
+                dialog.show(getFragmentManager(),"");
             }
         });
 
+    }
+    public void createNewNote(Note n) {
+
+        mNoteAdapter.addNote(n);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,5 +91,82 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public class NoteAdapter extends BaseAdapter {
 
-}
+        List<Note> noteList = new ArrayList<Note>();
+            @Override
+            public int getCount() {
+                return noteList.size();
+            }
+
+            public Note getItem(int whichItem) {
+                return noteList.get(whichItem);
+            }
+
+            @Override
+            public long getItemId(int whichItem) {
+                return whichItem;
+            }
+
+
+            @Override
+            public View getView(int whichItem, View view, ViewGroup viewGroup) {
+
+                //Test to see if view has been properly implemented
+
+                if( view == null) {
+
+                    //If not then we will do so here
+                    //by creating a LayoutInflater
+
+                    LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                    //Now we instantiate view using inflater.inflate using the listitem Layout
+
+                    view = inflater.inflate(R.layout.listitem, viewGroup, false);
+
+                    //false parameter is necessary because of the way we want to use listitem
+                }
+
+                //Grab a reference to all out textView and ImageView widgets
+
+                TextView txtTitle = (TextView) view.findViewById(R.id.txtTitle);
+                TextView txtDescription = (TextView) view.findViewById(R.id.txtDescription);
+                ImageView ivImportant = (ImageView) view.findViewById(R.id.imageViewImportant);
+                ImageView ivTodo = (ImageView) view.findViewById(R.id.imageViewTodo);
+                ImageView ivIdea = (ImageView) view.findViewById(R.id.imageViewIdea);
+
+                //Now, to hide any unwanted imageviews:
+
+                Note tempNote = noteList.get(whichItem);
+
+                if(!tempNote.isImportant()){
+                    ivImportant.setVisibility(View.GONE);
+                }
+
+                if(!tempNote.isTodo()){
+                    ivTodo.setVisibility(View.GONE);
+                }
+
+                if(!tempNote.isIdea()){
+                    ivIdea.setVisibility(View.GONE);
+                }
+
+                // Add the text to the heading and description
+
+                txtTitle.setText(tempNote.getTitle());
+                txtDescription.setText(tempNote.getDescription());
+
+                return view;
+                }
+
+                public void addNote (Note n){
+
+                    noteList.add(n);
+                    notifyDataSetChanged();
+                }
+
+            }
+        }
+
+
